253224cd4f1c7953cdc958bdc530e21543c93839
desarollologistica-png <desarollologistica@gmail.com>
2026-07-06 16:24:24 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QtrackingCartera from './QtrackingCartera'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QtrackingCartera />
+  </React.StrictMode>
+)
